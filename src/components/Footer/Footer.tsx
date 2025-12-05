09f0d02148cc682dce587a79e55ab5a98c9d40a0
Daniel Bruckner <bruckner.developer@gmail.com>
2025-12-05 16:02:48 +0100

    update footer github link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import frontEndMentor_logo from "/src/assets/frontendmentor.png"
 
 export default function() {
     return <footer className="footer">
-        <a href="https://github.com/BrucknerDeveloper" target="_blank">
+        <a href="https://github.com/daniel-bruckner" target="_blank">
             <img className="footer__icon" alt="github logo" src={github_logo} />
         </a>
         <a href="https://www.linkedin.com/in/daniel-bruckner-40b043257/" target="_blank">
